bb334af28b3e61a1e62810d9c16db4d15e2ebe21
Dmitriy Gorbunov <dmitriy.goto@gmail.com>
2022-05-28 00:21:48 +0300

    Rename java source folder to kotlin.

--- app-tv-server/build.gradle.kts
+++ app-tv-server/build.gradle.kts
@@ -24,6 +24,17 @@ android {
             )
         }
     }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
+
+        getByName("test") {
+            java.srcDirs("src/test/kotlin")
+        }
+    }
+
     packagingOptions {
         resources.excludes.add("META-INF/*")
     }
