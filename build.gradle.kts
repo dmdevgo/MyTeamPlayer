186fbd33f3ea53740261ccd48daa65cad1bab722
Dmitriy Gorbunov <dmitriy.goto@gmail.com>
2023-02-24 22:02:54 +0300

    Update Gardle and AGP.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.2.1" apply false
-    id("com.android.library") version "7.2.1" apply false
+    id("com.android.application") version "7.4.1" apply false
+    id("com.android.library") version "7.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.6.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21" apply false
     id("org.jetbrains.compose") version "1.2.0-alpha01-dev731" apply false
